b37e58bcf325ba1574a0d96f11b6266393d6741a
Bombonas <pablomejiast@gmail.com>
2023-11-13 18:30:36 +0100

    commit app 2

--- app/src/main/java/com/example/appcrazydisplay/MainActivity.java
+++ app/src/main/java/com/example/appcrazydisplay/MainActivity.java
@@ -2,7 +2,11 @@ package com.example.appcrazydisplay;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.Build;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            Window window = getWindow();
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+            window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+            window.setStatusBarColor(getResources().getColor(R.color.appColor));
+        }
+        ImageView logo = (ImageView) findViewById(R.id.imageView);
+        logo.setImageResource(R.drawable.morioh);
+        logo.getLayoutParams().width = 190*2;
+        logo.getLayoutParams().height = 247*2;
     }
 }
